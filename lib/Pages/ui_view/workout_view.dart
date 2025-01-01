29d502896acaee62fe5cab75c9a8621ea9650012
Hafize Sanlı <148492774+hafizesanli@users.noreply.github.com>
2025-01-01 12:18:54 +0300

    next exercise güncellendi

--- lib/Pages/ui_view/workout_view.dart
+++ lib/Pages/ui_view/workout_view.dart
@@ -1,12 +1,42 @@
 import 'package:first_app/main.dart';
 import 'package:flutter/material.dart';
 import '../fitness_app_theme.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class WorkoutView extends StatelessWidget {
   final AnimationController? animationController;
   final Animation<double>? animation;
 
-  const WorkoutView({super.key, this.animationController, this.animation});
+  const WorkoutView({
+    Key? key,
+    this.animationController,
+    this.animation,
+  }) : super(key: key);
+
+  Stream<String> getLastSelectedBodyPartStream() {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      return FirebaseFirestore.instance
+          .collection('Users')
+          .doc(user.uid)
+          .collection('Injuries')
+          .orderBy('timestamp', descending: true)
+          .limit(1)
+          .snapshots()
+          .map((snapshot) {
+            if (snapshot.docs.isNotEmpty) {
+              final data = snapshot.docs.first.data();
+              //print('Latest document data: $data');
+              //print('Timestamp: ${data['timestamp']}');
+              String muscleName = data['muscleName'] ?? 'No';
+              return muscleName[0].toUpperCase() + muscleName.substring(1);
+            }
+            return 'No';
+          });
+    }
+    return Stream.value('No');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,13 +69,13 @@ class WorkoutView extends StatelessWidget {
                         blurRadius: 10.0),
                   ],
                 ),
-                child: const Padding(
-                  padding: EdgeInsets.all(16.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                       Text(
+                      const Text(
                         'Next exercise',
                         textAlign: TextAlign.left,
                         style: TextStyle(
@@ -56,19 +86,25 @@ class WorkoutView extends StatelessWidget {
                           color: FitnessAppTheme.white,
                         ),
                       ),
-                       Padding(
-                        padding: EdgeInsets.only(top: 8.0),
-                        child: Text(
-                          'Shoulder impingement exercise',
-                          textAlign: TextAlign.left,
-                          style: TextStyle(
-                            fontFamily: FitnessAppTheme.fontName,
-                            fontWeight: FontWeight.normal,
-                            fontSize: 20,
-                            letterSpacing: 0.0,
-                            color: FitnessAppTheme.white,
-                          ),
-                        ),
+                      StreamBuilder<String>(
+                        stream: getLastSelectedBodyPartStream(),
+                        builder: (context, snapshot) {
+                          final bodyPart = snapshot.data ?? 'No';
+                          return Padding(
+                            padding: const EdgeInsets.only(top: 8.0),
+                            child: Text(
+                              '$bodyPart exercise',
+                              textAlign: TextAlign.left,
+                              style: const TextStyle(
+                                fontFamily: FitnessAppTheme.fontName,
+                                fontWeight: FontWeight.normal,
+                                fontSize: 20,
+                                letterSpacing: 0.0,
+                                color: FitnessAppTheme.white,
+                              ),
+                            ),
+                          );
+                        },
                       ),
                     ],
                   ),
